6a1b39d5811ce0ebc7dd5f859bf01b00edd538fd
kaisewhite <kaisewhite@kais-app.com>
2021-01-11 22:04:18 -0500

    replaced secrets function

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var client = new AWS.SecretsManager({
 
 /******************** RETRIEVE SECRET FROM SECRETS MANAGER ***************************/
 // Call the AWS API and return a Promise
-const getAwsSecret = (secretName) => {
+/**const getAwsSecret = (secretName) => {
   return client.getSecretValue({ SecretId: secretName }).promise();
 };
 // Create a async function to use the Promise
@@ -24,6 +24,16 @@ const getSecretValues = async (secretName) => {
   const data = secret.SecretString;
   //console.log(`Retreiving data from secret: ${data}`);
   return data;
+}; */
+
+const AWSSecretsManager = async (secretName) => {
+  const response = await client.getSecretValue({ SecretId: secretName }).promise();
+
+  if ("SecretString" in response) {
+    return response.SecretString;
+  } else {
+    return Buffer.from(response.SecretBinary, "base64").toString("ascii");
+  }
 };
 
 /******************** DATABASE  *********************/
@@ -33,7 +43,7 @@ const getSecretValues = async (secretName) => {
  * feidls - this is an object that contains the fields to be inserted into the table
  */
 const insertIntoSQLDatabase = async (secret, fields) => {
-  const config = await getSecretValues(secret).then((data) => {
+  const config = await AWSSecretsManager(secret).then((data) => {
     return JSON.parse(data);
   });
 
@@ -80,7 +90,39 @@ const processLogDataProd = (event) => {
   // If the logstream equals ec2/gasatraq/i-01414565ae2db9bed/GASATRAQ_WEB/W3SVC3-iislogs
   if (parsed.logStream === productionLogStream) {
     console.log("Displaying Log Stream for Production:", JSON.stringify(parsed.logStream));
-    console.log(JSON.stringify(parsed.logEvents));
+    //console.log(JSON.stringify(parsed.logEvents));
+    const LogEventsArray = parsed.logEvents;
+    //Loop through every item in the array
+    for (let item of LogEventsArray) {
+      const fields = {
+        Id: uuidv4(),
+        HTTPMethod: item.message.match(HTTPMethodPattern).toString(),
+        date: item.message.match(datePattern)[0].toString(),
+        timeStamp: item.message.match(timePattern).toString(),
+        path: item.message.split(HTTPMethodPattern)[1].split("-")[0].replace(/\s/g, ""), //Removes white spaces,
+        URL: item.message.match(URLPattern) === null ? "N/A" : item.message.match(URLPattern).toString(),
+        IPAddress: item.message.split("").reverse().join("").match(IPAddressPattern).toString().split("").reverse().join(""),
+      };
+      //ignore healthchecks so we don't dump those ito the database
+      if (item.message.includes("ELB-HealthChecker") || item.message.includes("Amazon-Route53")) {
+        console.log("Ignoring HealthChecks");
+      } else {
+        //Pass in secret and fields object
+        insertIntoSQLDatabase(secret, fields);
+      }
+    }
+  }
+};
+
+const processLogDataStag = (event) => {
+  const payload = Buffer.from(event.awslogs.data, "base64");
+  const parsed = JSON.parse(zlib.gunzipSync(payload).toString("utf8"));
+  const secret = "GasatraqStagingMSSQLRDSSecret"; //Name of the secret to retrieve database creds
+
+  // If the logstream equals ec2/gasatraq/i-01414565ae2db9bed/GASATRAQ_WEB/W3SVC3-iislogs
+  if (parsed.logStream === stagingLogStream) {
+    console.log("Displaying Log Stream for Dev:", JSON.stringify(parsed.logStream));
+    //console.log(JSON.stringify(parsed.logEvents));
     const LogEventsArray = parsed.logEvents;
     //Loop through every item in the array
     for (let item of LogEventsArray) {
@@ -112,7 +154,7 @@ const processLogDataDev = (event) => {
   // If the logstream equals ec2/gasatraq/i-01414565ae2db9bed/GASATRAQ_WEB/W3SVC3-iislogs
   if (parsed.logStream === devLogStream) {
     console.log("Displaying Log Stream for Dev:", JSON.stringify(parsed.logStream));
-    console.log(JSON.stringify(parsed.logEvents));
+    //console.log(JSON.stringify(parsed.logEvents));
     const LogEventsArray = parsed.logEvents;
     //Loop through every item in the array
     for (let item of LogEventsArray) {
@@ -136,8 +178,13 @@ const processLogDataDev = (event) => {
   }
 };
 
-exports.handler = (event, context, callback) => {
+exports.handler = (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  processLogDataProd(event);
+  const payload = Buffer.from(event.awslogs.data, "base64");
+  const parsed = JSON.parse(zlib.gunzipSync(payload).toString("utf8"));
+  console.log(JSON.stringify(parsed));
+
   processLogDataDev(event);
+  processLogDataStag(event);
+  processLogDataProd(event);
 };
